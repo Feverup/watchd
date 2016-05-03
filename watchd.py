b588919e4822d839c0d7892469235eb12bf25d6f
Javier Palacios <javier.palacios@feverup.com>
2016-05-03 16:51:47 +0000

    Change watcher behaviour to care about ELBs instead of single instances

--- watchd.py
+++ watchd.py
@@ -2,6 +2,9 @@
 
 import rrdtool
 
+import boto.ec2
+import boto.ec2.elb
+
 import array
 import math
 import os
@@ -11,25 +14,34 @@ topdir = '/var/lib/collectd/rrd/'
 if __name__ == '__main__' :
 
     if len(os.sys.argv) != 2 :
-        print "Usage: %s hostname" % os.sys.argv[0].split('.')[-1]
+        print "Usage: %s elbname" % os.sys.argv[0].split('.')[-1]
         os.sys.exit(2)
 
-    hostname = os.sys.argv[1]
-    rrdfile = os.path.join( topdir , hostname , 'cpu-0' , 'cpu-idle.rrd' )
+    elbname = os.sys.argv[1]
+    elb = boto.ec2.elb.connect_to_region("eu-west-1") \
+                      .get_all_load_balancers([elbname])[0]
 
-    info = rrdtool.info( rrdfile )
-    last = info['last_update'] - info['last_update'] % 60
-    data = rrdtool.fetch( rrdfile, 'AVERAGE', '--resolution' , '60' ,
-                         '--start' , '-10m' , '--end' , str(last) )
+    in_service = [ i.instance_id for i in elb.get_instance_health() if i.state == 'InService' ]
+    instances = boto.ec2.connect_to_region("eu-west-1") \
+                        .get_only_instances(in_service)
 
-    data = array.array( 'f' , [ d[0] for d in data[2] if d[0] ] )
+    for hostname in [ str(i.private_dns_name.split('.')[0]) for i in instances ] :
 
-    n = len(data)
-    mean = sum(data) / n
-    data2 = [ v*v for v in data ]
-    sd  = math.sqrt( sum(data2) / n - mean*mean )
+        rrdfile = os.path.join( topdir , hostname , 'cpu-0' , 'cpu-idle.rrd' )
 
-    # As indexes start at 0, we use floor instead of ceil for percentile index
-    limit = int(math.floor( n * 0.2 ))
-    minval = sorted(data)[limit]
+        info = rrdtool.info( rrdfile )
+        last = info['last_update'] - info['last_update'] % 60
+        data = rrdtool.fetch( rrdfile, 'AVERAGE', '--resolution' , '60' ,
+                              '--start' , '-10m' , '--end' , str(last) )
+
+        data = array.array( 'f' , [ d[0] for d in data[2] if d[0] ] )
+
+        n = len(data)
+        mean = sum(data) / n
+        data2 = [ v*v for v in data ]
+        sd  = math.sqrt( sum(data2) / n - mean*mean )
+
+        # As indexes start at 0, we use floor instead of ceil for percentile index
+        limit = int(math.floor( n * 0.2 ))
+        minval = sorted(data)[limit]
 
