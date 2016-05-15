6482089fbf988321788edd7833f064fedcd9d314
Javier Palacios <javier.palacios@feverup.com>
2016-05-15 22:56:18 +0000

    Add a class to hold recent metric values

--- fevertools/metrics.py
+++ fevertools/metrics.py
@@ -14,3 +14,23 @@ def recv( sock , buffsize=1024 ) :
         size -= len(buff.split('\n'))
     return data.split('\n')[1:-1]
 
+class aggregated_metric ( dict ) :
+
+    def __init__ ( self , length=10 ) :
+        self.length = length
+        dict.__init__( self )
+
+    def unshift ( self ) :
+        keys = self.keys()
+        keys.sort()
+        keys.reverse()
+        return dict.pop(self, keys.pop())
+
+    def __setitem__ ( self , key , value ) :
+        dict.__setitem__( self , "%d"%float(key) , value )
+        if len(self) > self.length :
+            self.unshift()
+
+    def __str__ ( self ) :
+        return "size: %d\n%s" % ( len(self) , "\n".join( [ "%s %s" % ( k , self[k] ) for k in self.keys() ] ) )
+
