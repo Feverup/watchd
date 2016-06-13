a83a734d4ebf219192ef3e325a4730334fc07439
Javier Palacios <javier.palacios@feverup.com>
2016-06-13 15:57:59 +0000

    Create an instance for every metric in configuration file

--- watchd.py
+++ watchd.py
@@ -18,8 +18,8 @@ unixsock = '/var/run/collectd-unixsock'
 
 if __name__ == '__main__' :
 
-  if len(os.sys.argv) != 2 :
-      print "Usage: %s section" % os.sys.argv[0].split('.')[-1]
+  if len(os.sys.argv) != 1 :
+      print "Usage: %s" % os.sys.argv[0].split('.')[-1]
       os.sys.exit(2)
 
   if os.fork() :
@@ -27,17 +27,16 @@ if __name__ == '__main__' :
 
   config = ConfigParser.ConfigParser()
   config.read( [ 'watchd.ini' , '/etc/watchd.ini' ] )
-  if not config.has_section( os.sys.argv[1] ) :
-      print "ERROR: no section named '%s' on onfiguration file" % os.sys.argv[1]
-      os.sys.exit(1)
 
-  metric_list = config.get( os.sys.argv[1] , 'metric_list' ).split()
-  elbname = config.get( os.sys.argv[1] , 'elbname' )
-  threshold = config.getfloat( os.sys.argv[1] , 'threshold' )
-  policy = config.get( os.sys.argv[1] , 'policy' )
-  statistics = config.get( os.sys.argv[1] , 'statistics' )
+  metrics = []
+  for name in config.sections() :
+      metric_list = config.get( name , 'metric_list' ).split()
+      elbname = config.get( name , 'elbname' )
+      threshold = config.getfloat( name , 'threshold' )
+      policy = config.get( name , 'policy' )
+      statistics = config.get( name , 'statistics' )
 
-  metrics = aggregated_metric(statistics)
+      metrics.append( aggregated_metric(statistics) )
 
   while True :
 
