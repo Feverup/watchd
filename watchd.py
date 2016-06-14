47cbf1163ff64d31a157e1f245566c47901aa1f8
Javier Palacios <javier.palacios@feverup.com>
2016-06-14 15:06:10 +0000

    Shift to YAML configuration

--- watchd.py
+++ watchd.py
@@ -7,7 +7,7 @@ import boto.ec2
 import boto.ec2.elb
 import boto.ec2.autoscale
 
-import ConfigParser
+import yaml
 
 import socket
 import time
@@ -25,17 +25,20 @@ if __name__ == '__main__' :
   if os.fork() :
       os.sys.exit(0)
 
-  config = ConfigParser.ConfigParser()
-  config.read( [ 'watchd.ini' , '/etc/watchd.ini' ] )
-  if not config.has_section( os.sys.argv[1] ) :
+  config = 'watchd.yml'
+  if not os.path.isfile( config ) :
+      conf_file = os.path.join( '/etc' , config )
+  with open( config ) as fd :
+      config = yaml.load( fd )
+  if not config.has_key( os.sys.argv[1] ) :
       print "ERROR: no section named '%s' on onfiguration file" % os.sys.argv[1]
       os.sys.exit(1)
 
-  metric_list = config.get( os.sys.argv[1] , 'metric_list' ).split()
-  elbname = config.get( os.sys.argv[1] , 'elbname' )
-  threshold = config.getfloat( os.sys.argv[1] , 'threshold' )
-  policy = config.get( os.sys.argv[1] , 'policy' )
-  statistics = config.get( os.sys.argv[1] , 'statistics' )
+  metric_list = config[os.sys.argv[1]]['metric_list']
+  elbname = config[os.sys.argv[1]]['elbname']
+  threshold = float(config[os.sys.argv[1]]['threshold'])
+  policy = config[os.sys.argv[1]]['policy']
+  statistics = config[os.sys.argv[1]]['statistics']
 
   metrics = aggregated_metric(statistics)
 
