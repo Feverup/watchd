bac9477604b5330b788222ba819979de4fc407c3
Javier Palacios <javier.palacios@feverup.com>
2016-07-07 10:12:20 +0000

    Write to logfile even when no new data is included on calculations

--- fevertools/metrics.py
+++ fevertools/metrics.py
@@ -286,7 +286,7 @@ class aggregated_elb ( aggregated_metric ) :
                 if data :
                     self[date] = data.split('=')[1]
 
-        if self.check_date(date) and len(self.last(60*self.window)) and self.logfile :
+        if len(self.last(60*self.window)) and self.logfile :
             with open( '%s.out' % self.elbname , 'a+' ) as fd :
                 fd.write( "%s %14.2f %s\n" % ( datetime.datetime.now() , date , self.dump(60*self.window) ) )
 
