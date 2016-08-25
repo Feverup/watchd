d2ffc00de712f179c3052f936326f1e0172f7895
Javier Palacios <javier.palacios@feverup.com>
2016-08-25 14:44:03 +0000

    Handle socket errors on object to query collectd

--- watchd/datastore.py
+++ watchd/datastore.py
@@ -1,11 +1,16 @@
 
 import socket
+import datetime
+import sys
 
 class collectd :
 
     def __init__ ( self , sockname="/var/run/collectd-unixsock" ) :
         self.sock = socket.socket( socket.AF_UNIX )
-        self.sock.connect( sockname )
+        try :
+            self.sock.connect( sockname )
+        except socket.error, ex :
+            sys.stderr.write( "ERROR : %s , socket failed to connect; %s\n" % ( datetime.datetime.now() , ex ) )
 
     def get ( self , payload ) :
         return self.__send("GETVAL %s\n" % payload)
@@ -14,25 +19,34 @@ class collectd :
         return self.__send("PUTVAL %s\n" % payload)
 
     def __send ( self , payload , buffsize=1024 ) :
-        self.sock.send( payload )
-        data = self.sock.recv(buffsize)
-        while data.find(' ') < 0 :
-            data += self.sock.recv(buffsize)
+        try :
+            self.sock.send( payload )
+        except socket.error, ex :
+            sys.stderr.write( "ERROR : %s , socket failed to send; %s\n" % ( datetime.datetime.now() , ex ) )
+        try :
+            data = self.sock.recv(buffsize)
+            while data.find(' ') < 0 :
+                data += self.sock.recv(buffsize)
+        except socket.error, ex :
+            sys.stderr.write( "ERROR : %s , socket failed to read; %s\n" % ( datetime.datetime.now() , ex ) )
         size = data.split()[0]
         if size == '-1' :
           size = 0
         size = int(size) + 2 # Add header and trailing newline
-        while len(data.split('\n')) < size :
-            while data[-1] != "\n" :
-                data += self.sock.recv(buffsize)
-            if len(data.split('\n')) < size :
-                data += self.sock.recv(buffsize)
+        try :
+            while len(data.split('\n')) < size :
+                while data[-1] != "\n" :
+                    data += self.sock.recv(buffsize)
+                if len(data.split('\n')) < size :
+                    data += self.sock.recv(buffsize)
+        except socket.error, ex :
+            sys.stderr.write( "ERROR : %s , socket failed to read; %s\n" % ( datetime.datetime.now() , ex ) )
         items = data.split('\n')
         items.pop() # Remove trailing endline
         response_size , status_line = items.pop(0).split(None, 1)
         if size == 2 :
             if response_size == '-1' :
-                os.sys.stderr.write( "ERROR : %s , '%s %s' gave %s\n" % ( datetime.datetime.now() , command , payload , status_line ) )
+                sys.stderr.write( "ERROR : %s , '%s' gave %s\n" % ( datetime.datetime.now() , payload , status_line ) )
             return
         elif size == 3 :
             return items[0]
